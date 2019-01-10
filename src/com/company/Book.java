60f06743f3fe5af8ed4e9d2e48cc1915d110f83f
Awet Fessha <awetkifle94@gmail.com>
2019-01-10 23:49:07 +0000

    add getters and seeters for book class

--- src/com/company/Book.java
+++ src/com/company/Book.java
@@ -1,4 +1,38 @@
 package com.company;
 
 public class Book {
+private  String title, ISDN;
+private int numberOfPages;
+
+
+//Getters and setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getISDN() {
+        return ISDN;
+    }
+
+    public void setISDN(String ISDN) {
+        this.ISDN = ISDN;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    //Book details
+    public void bookDetails(int i, int n, int k){
+
+    }
 }
