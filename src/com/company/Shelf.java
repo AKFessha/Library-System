1d0d31c745326b901aa5d929a4dfa7b7e83ded45
Awet Fessha <awetkifle94@gmail.com>
2019-01-10 23:54:27 +0000

    add getters and seeters for Shelf  class

--- src/com/company/Shelf.java
+++ src/com/company/Shelf.java
@@ -1,4 +1,46 @@
 package com.company;
 
 public class Shelf {
+    private int number, length;
+    private String shelfName;
+    Book[] myBooks= new Book [2];
+
+    //Getters and setters
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public String getShelfName() {
+        return shelfName;
+    }
+
+    public void setShelfName(String shelfName) {
+        this.shelfName = shelfName;
+    }
+
+    public Book[] getMyBooks() {
+        return myBooks;
+    }
+
+    public void setMyBooks(Book[] myBooks) {
+        this.myBooks = myBooks;
+    }
+
+    //Shelf details
+    public void shelfDetails(int i,int n, String cabinetTitle){
+
+    }
 }
