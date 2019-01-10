6476d41ed7fbebb0b9e78eeacb814e4cc5f6b7bf
Awet Fessha <awetkifle94@gmail.com>
2019-01-10 23:58:42 +0000

    add getters and seeters for Cabinet  class

--- src/com/company/Cabinet.java
+++ src/com/company/Cabinet.java
@@ -1,4 +1,60 @@
 package com.company;
 
 public class Cabinet {
+    private String CabinetTitle, color;
+    private int number, width, height;
+
+    Shelf[] myShelves= new Shelf[2];
+
+    //Getters and setters
+
+    public String getCabinetTitle() {
+        return CabinetTitle;
+    }
+
+    public void setCabinetTitle(String cabinetTitle) {
+        CabinetTitle = cabinetTitle;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public Shelf[] getMyShelves() {
+        return myShelves;
+    }
+
+    public void setMyShelves(Shelf[] myShelves) {
+        this.myShelves = myShelves;
+    }
+
+
 }
